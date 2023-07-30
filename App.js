071e008972e152b70d42f7f18cdd59b35c3895c4
PALLAVI B <119939134+Pallavibu@users.noreply.github.com>
2023-07-30 17:19:42 +0000

    first commit

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading = React.createElement("h1", {}, "Hello Pallavi WelCome to React" );
+const parent = React.createElement(
+    "div", 
+    {id : "parent"}, 
+    [
+        React.createElement("div", {id : "child1"}, [
+            React.createElement("h1", {}, "Hello World in React"), 
+            React.createElement("h2", {}, "Hello World in React2")
+        ]),
+        React.createElement("div", {id : "child2"}, [
+            React.createElement("h1", {}, "Hello World in React"), 
+            React.createElement("h2", {}, "Hello World in React2")
+        ])
+    ]
+    );
+
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
