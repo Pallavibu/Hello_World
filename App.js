15ecd80c0f07675c1732bf4e649a5f23776dd957
Pallavi B <pallavibu44@gmail.com>
2023-08-05 18:51:25 +0530

    dependencies added

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-const parent = React.createElement(
-    "div", 
-    {id : "parent"}, 
-    [
-        React.createElement("div", {id : "child1"}, [
-            React.createElement("h1", {}, "Hello World in React"), 
-            React.createElement("h2", {}, "Hello World in React2")
-        ]),
-        React.createElement("div", {id : "child2"}, [
-            React.createElement("h1", {}, "Hello World in React"), 
-            React.createElement("h2", {}, "Hello World in React2")
-        ])
-    ]
-    );
+import React from "react";
+import ReactDOM from "react-dom";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hello World in React"),
+    React.createElement("h2", {}, "Hello World in React2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello World in React"),
+    React.createElement("h2", {}, "Hello World in React2"),
+  ]),
+]);
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "Hello World in React"),
+        React.createElement("h2", {}, "Hello World in React2")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "Hello World in React"),
+        React.createElement("h2", {}, "Hello World in React2")
+    ])
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
